049f08a5b001904a158e721d1cd47a5bc39ed217
bencouser <neb2010@live.co.uk>
2024-01-26 21:05:23 +0000

    starting again

--- scripts/create_baseline.py
+++ scripts/create_baseline.py
@@ -6,6 +6,28 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def naive_forecast(dataframe):
+    """
+    Naive Forecasting.
+    Shift only the 'Close' column by one position downwards,
+    duplicating the first value of the 'Close' column.
+    All other column values remain unchanged.
+    The last row is dropped after the shift.
+    """
+    predictions = dataframe.copy()
+
+    # Save the first value of the 'Close' column
+    first_close_value = dataframe['Close'].iloc[0]
+
+    # Shift the 'Close' column by one position downwards
+    predictions['Close'] = dataframe['Close'].shift(1)
+
+    # Replace the first value of the shifted 'Close' column
+    predictions['Close'].iloc[0] = first_close_value
+
+    return pd.DataFrame(predictions)
+
+
 df = pd.read_csv(
     '../data/processed/Bitcoin_Closing_Prices.csv',
     index_col=0)
@@ -16,22 +38,19 @@ train_size = int(len(df) * 0.9)
 train, test = df[0:train_size], df[train_size:len(df)]
 
 
-# make a copy of test set
-test_copy = test.copy()
+predictions = naive_forecast(test)
+
+print("Real Values")
+print(test.head())
+print("Naive Predictions")
+print(predictions.head())
+
+print(test)
+print(predictions)
 
 
-# Naive forcast: use last value from training set to predict
-last_value = train.iloc[-1]['Close']
-naive_prediction = [last_value] * len(test_copy)
-
-
-# Compare Predictions to Actual Values
-test_copy.loc[:, 'naive_predictions'] = naive_prediction
-
-
-mae = mean_absolute_error(test_copy['Close'], test_copy['naive_predictions'])
+mae = mean_absolute_error(test['Close'], predictions['Close'])
 print('MAE: %f' % mae)
-
 
 # # Plotting the actual and predicted values
 # plt.figure(figsize=(10, 6))
@@ -46,23 +65,23 @@ print('MAE: %f' % mae)
 # plt.show()
 
 # Downsampling for plotting: taking every 10th value
-downsampled_test = test_copy.iloc[::10]
-
-# Plotting the actual and predicted values with downsampling
-plt.figure(figsize=(10, 6))
-plt.plot(downsampled_test.index,
-         downsampled_test['Close'],
-         label='Actual Values',
-         marker='o')
-plt.plot(downsampled_test.index,
-         downsampled_test['naive_predictions'],
-         label='Naive Predictions',
-         color='red',
-         linestyle='--',
-         marker='x')
-plt.title('Naive Forecast vs Actual Data (Downsampled)')
-plt.xlabel('Time')
-plt.ylabel('Bitcoin Price')
-plt.legend()
-plt.savefig('../images/NiaveForecastPrediction_Downsampled.png')
-plt.show()
+# downsampled_test = test_copy.iloc[::10]
+#
+# # Plotting the actual and predicted values with downsampling
+# plt.figure(figsize=(10, 6))
+# plt.plot(downsampled_test.index,
+#          downsampled_test['Close'],
+#          label='Actual Values',
+#          marker='o')
+# plt.plot(downsampled_test.index,
+#          downsampled_test['naive_predictions'],
+#          label='Naive Predictions',
+#          color='red',
+#          linestyle='--',
+#          marker='x')
+# plt.title('Naive Forecast vs Actual Data (Downsampled)')
+# plt.xlabel('Time')
+# plt.ylabel('Bitcoin Price')
+# plt.legend()
+# plt.savefig('../images/NiaveForecastPrediction_Downsampled.png')
+# plt.show()
