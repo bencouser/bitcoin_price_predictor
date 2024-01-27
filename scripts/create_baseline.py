6212dbae11fe68744022a0bdd41f8a650c07d404
bencouser <neb2010@live.co.uk>
2024-01-27 07:57:14 +0000

    niave forcast done

--- scripts/create_baseline.py
+++ scripts/create_baseline.py
@@ -40,48 +40,30 @@ train, test = df[0:train_size], df[train_size:len(df)]
 
 predictions = naive_forecast(test)
 
-print("Real Values")
-print(test.head())
-print("Naive Predictions")
-print(predictions.head())
-
-print(test)
-print(predictions)
-
 
 mae = mean_absolute_error(test['Close'], predictions['Close'])
 print('MAE: %f' % mae)
 
-# # Plotting the actual and predicted values
-# plt.figure(figsize=(10, 6))
-# plt.plot(test.index, test['Close'], label='Actual Values')
-# plt.plot(test_copy.index, test_copy['naive_predictions'],
-#          label='Naive Predictions', color='red', linestyle='--')
-# plt.title('Naive Forecast vs Actual Data')
-# plt.xlabel('Time')
-# plt.ylabel('Bitcoin Price')
-# plt.legend()
-# plt.savefig('../images/NiaveForecastPrediction(downsampled).png')
-# plt.show()
 
 # Downsampling for plotting: taking every 10th value
-# downsampled_test = test_copy.iloc[::10]
-#
-# # Plotting the actual and predicted values with downsampling
-# plt.figure(figsize=(10, 6))
-# plt.plot(downsampled_test.index,
-#          downsampled_test['Close'],
-#          label='Actual Values',
-#          marker='o')
-# plt.plot(downsampled_test.index,
-#          downsampled_test['naive_predictions'],
-#          label='Naive Predictions',
-#          color='red',
-#          linestyle='--',
-#          marker='x')
-# plt.title('Naive Forecast vs Actual Data (Downsampled)')
-# plt.xlabel('Time')
-# plt.ylabel('Bitcoin Price')
-# plt.legend()
-# plt.savefig('../images/NiaveForecastPrediction_Downsampled.png')
-# plt.show()
+downsampled_predictions = predictions.iloc[::10]
+downsampled_test = test.iloc[::10]
+
+# Plotting the actual and predicted values with downsampling
+plt.figure(figsize=(10, 6))
+plt.plot(downsampled_test.index,
+         downsampled_test['Close'],
+         label='Actual Values',
+         marker='o')
+plt.plot(downsampled_predictions.index,
+         downsampled_predictions['Close'],
+         label='Naive Predictions',
+         color='red',
+         linestyle='--',
+         marker='x')
+plt.title('Naive Forecast vs Actual Data (Downsampled)')
+plt.xlabel('Time')
+plt.ylabel('Bitcoin Price')
+plt.legend()
+plt.savefig('../images/NiaveForecastPrediction_Downsampled.png')
+plt.show()
